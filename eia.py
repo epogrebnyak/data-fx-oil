01415c75e81387b36e10b13ec1ebdf8987b106bb
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2015-12-07 18:25:23 +0300

    Update eia.py

--- eia.py
+++ eia.py
@@ -1,52 +1,114 @@
-""" Download Brent FOB price time series from EIA API. """
-
+# ----------------------------
+#   Data sample:
+# ----------------------------
 
 # this is a truncated version of http://api.eia.gov/series/?api_key=15C0821C54636C57209B84FEEE3CE654&series_id=PET.RBRTE.D
-xml_doc = """{"request":{"command":"series","series_id":"PET.RBRTE.D"},"series":[{"series_id":"PET.RBRTE.D","name":"Europe Brent Spot Price FOB, Daily","units":"Dollars per Barrel","f":"D","unitsshort":"$\/bbl","description":"Europe Brent Spot Price FOB","copyright":"Thomson-Reuters","source":"Thomson-Reuters","start":"19870520","end":"20151201","updated":"2015-12-02T13:20:30-0500",
-"data":[["20151201",42.97],["20151130",43.73],["20151127",43.07],["20151126",43.55],["20151125",43.56],["20151124",44.38],["20151123",43.7],["20151120",42.49],["20151119",42.22],["20151118",41.45],["20151117",41.28],["20151116",40.28],["20151113",41.98],["19870521",18.45],["19870520",18.63]]}]}"""
+XML_DOC = """{"request":{"command":"series","series_id":"PET.RBRTE.D"},
+"series":[{"series_id":"PET.RBRTE.D","name":"Europe Brent Spot Price FOB, Daily","units":"Dollars per Barrel","f":"D","unitsshort":"$\/bbl","description":"Europe Brent Spot Price FOB","copyright":"Thomson-Reuters","source":"Thomson-Reuters","start":"19870520","end":"20151201","updated":"2015-12-02T13:20:30-0500","data":[["20151201",42.97],["20151130",43.73],["20151127",43.07],["20151126",43.55],["20151125",43.56],["20151124",44.38],["20151123",43.7],["20151120",42.49],["20151119",42.22],["20151118",41.45],["20151117",41.28],["20151116",40.28],["20151113",41.98],["19870521",18.45],["19870520",18.63]]}]}"""
 
-# source: http://www.diveintopython3.net/xml.html
-# import xml.etree.ElementTree as etree
-# tree = etree.parse('examples/feed.xml')
-# root = tree.getroot()
-# root
+# ----------------------------
+#   Code:
+# ----------------------------
+
+""" Download Brent FOB price time series from EIA API. """
+
+from urllib.request import urlopen
+import json
 
 SOURCE_URL = "http://api.eia.gov/series/?api_key=15C0821C54636C57209B84FEEE3CE654&series_id=PET.RBRTE.D"
 
-def iter_raw_eia_brent_fob():
-    # TODO: this function must return data from EIA on Brent FOB Price, retrieved from SOURCE_URL 
-    #       result must pass test_iter()
+def get_restful_data(url = SOURCE_URL):
+    # Download data from SOURCE_URL 
+    with urlopen(url) as f:
+        url_data = f.read().decode()
+    return url_data 
+ 
+def parse_as_json_and_iterate(url_data):
+    # Parse url_data and emit values
+    json_data = json.loads(url_data)    
+    for x in json_data["series"][0]["data"]:
+        yield x
+
+# NOTE: may also use lxml or xmltodict for parsing, eg:
+# import xmltodict
+# data = xmltodict.parse(data)"""
+
+def parse(url_data):
+    """Returns a list of timeseries values (timestamp, value)"""
+    return list(parse_as_json_and_iterate(url_data)) 
+    # NOTE: may also apply sorting here
     
-    # Download data from SOURCE_URL to xml_doc - maybe using urllib2
-    # 
+def raw_eia_brent_fob():
+    """Yeilds data data from EIA on Brent FOB Price retrieved from SOURCE_URL
+       Passes test_iter()"""
+    url_data = get_restful_data()
+    return parse(url_data)
+   
+# ----------------------------
+#   Testing:
+# ----------------------------   
+
+def raw_eia_brent_fob_local_copy(url_data = XML_DOC):
+    return parse(url_data)
     
-    # Parse xml_doc and emit values - maybe using xmltodict
-    # ...
-    
-    # stub
-    yield ("20151201",42.97)
-    yield ("20151130",43.73)    
-    yield ("20151127",43.07)
+def check_values_from_eia_brent_fob(list_):
+    """Checks few entries *list_* """
+    test_values =[["20151201",42.97], ["20151130",43.73], ["20151127",43.07]]
+    for t in test_values:
+       assert t in list_    
+    print("Test passed OK.")    
     
 def test_iter():
-    gen = iter_raw_eia_brent_fob()
-    test_dict = dict((x[0],x[1]) for x in gen)
-    assert test_dict["20151201"] == 42.97
-    assert test_dict["20151130"] == 43.73
-    assert test_dict["20151127"] == 43.07
+    # urlopen() fails behind firewall, so I test only the XML/JSON parser
+    # gen = raw_eia_brent_fob()
+    gen = raw_eia_brent_fob_local_copy()
+    check_values_from_eia_brent_fob(gen)
     
 if __name__ == "__main__":
     test_iter()
     
-"""Suggested code:
 
-import urllib2
-import xmltodict
-def homepage(request):
-    file = urllib2.urlopen('http://api.eia.gov/series/?api_key=15C0821C54636C57209B84FEEE3CE654&series_id=PET.RBRTE.D')
-    data = file.read()
-    file.close()
-    data = xmltodict.parse(data)
-    return render_to_response('my_template.html', {'data': data})
-    
+# ----------------------------
+#   For reference:
+# ----------------------------
+
+"""Sample output (whitespaces added below for readability). From http://www.eia.gov/opendata/commands.cfm#series_query
+
+{
+"request":{
+    "command":"series",
+    "series_id":"ELEC.GEN.ALL-AK-99.A"
+    },
+
+"series":[
+        {
+        "series_id":"ELEC.GEN.ALL-AK-99.A"
+        "name":"Net Generation : All Fuels : Alaska : All Sectors",
+        "units":"thousand megawatthours",
+        "f":"A",
+        "unitsshort":"",
+        "description":"Summation of all fuels used for electricity generation; All sectors; ",
+        "copyright":"None",
+        "source":"EIA, U.S. Energy Information Administration",
+        "iso3166":"USA-AK", DEPRECATED
+        "lat":"", DEPRECATED
+        "lon":"", DEPRECATED
+        "latlon":"43.5589,-91.2325", 
+        "latlon2":"43.5589,-91.2325", 
+        "geography":"USA-AK", 
+        "geography2":"USA-AK", 
+        "lastHistoricalPeriod":"2013", 
+        "start":"2001",
+        "end":"2013",
+        "updated":"2013-08-23T17:37:44-0400",
+        "data":[
+        ["2012",6979.39223],
+        ["2011",6871.03279],
+        ["2010",6759.5757],
+        ["2009",6702.15939],
+        ["2008",6774.83438],
+        ...
+        }
+    ]
+}
 """
